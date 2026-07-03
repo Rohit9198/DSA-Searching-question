a1ee3f7d040e98520fff0bfe8baa0defee949b8d
ROHIT SINGH <rohitsi2252@gmail.com>
2026-07-03 13:24:58 +0530

    1

--- src/Binary1.java
+++ src/Binary1.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class Binary1 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,2,3,4};
+        int target = 2;
+
+        int ans = Binary1(arr, target);
+        System.out.println(ans);
+    }
+
+    static int Binary1(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                ans = mid;
+                start = mid+1;
+            }
+        }
+
+        return ans;
+    }
 }
--- src/linear1.java
+++ src/linear1.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class linear1 {
+    static int linearSearch(int []arr, int target){
+        if(arr.length == 0){
+            return -1;
+        }
+        for (int index=0; index < arr.length; index++){
+            int element = arr[index];
+            if(element == target){
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {12, 13, 34, 45, 56, 78, 89, 90};
+        int target = 78;
+        int ans = linearSearch(nums, target);
+        System.out.println(ans);
+    }
 }
